d22dba21b2ffa6d4b105a6f53d48993bfe236ab9
MissChenduo <zdt5500@autuni.ac.nz>
2022-08-15 22:33:14 +1200

    Update index.js
    Update index,js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
+import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./App";
- 
-ReactDOM.render(
-  <App/>, 
-  document.getElementById("root")
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
